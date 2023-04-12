57d1c9cc45899c685b1e349682d124fe98d6e1dc
Kairat <kairat1528@MacBook-Air-Kairat.local>
2023-04-12 20:12:32 +0600

    addcontacts

--- src/App.js
+++ src/App.js
@@ -6,72 +6,43 @@ import Total from './components/Total/Total';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const getPerson = () => {
+const getContacts = () => {
   return axios.get('http://localhost:3001/contacts')
 }
 
-function App() {
-  // const course = [
-  //   {
-  //     id: 1,
-  //     name: "IT-RUN React Web Dev",
-  //     tasks: [
-  //       {
-  //         part: "Вводный курс по React",
-  //         task: 7
-  //       },
-  //       {
-  //         part: "Состояние компонента",
-  //         task: 15
-  //       },
-  //       {
-  //         part: "Декомпозиция компонентов",
-  //         task: 10
-  //       },
-  //     ]
-  //   },
-  //   {
-  //     id: 2,
-  //     name: "IT-RUN Python Web Dev",
-  //     tasks: [
-  //       {
-  //         part: "Вводный курс по Python",
-  //         task: 5
-  //       },
-  //       {
-  //         part: "Переменные циклы Python",
-  //         task: 12
-  //       },
-  //       {
-  //         part: "Фреймворк Django",
-  //         task: 17
-  //       },
-  //     ]
-  //   }
-  // ]
-  const [persons, setPersons] = useState([{
-    id: 1,
-    name: "Kairat Murataliev",
-    number: "996702541269"
-  }])
-  const [newName, setNewName] = useState('')
+const createPost = (person) => {
+  return axios.post('http://localhost:3001/contacts', person)
+}
 
-  useEffect(() => { 
-    getPerson() 
-    .then(res =>  
-      setPersons(res.data)) 
-  }, []) 
+function App() {
+
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState("")
+  const [newNumber, setNewNumber] = useState("");
+
+  useEffect(() => {
+    getContacts()
+      .then(res =>
+        setPersons(res.data))
+  }, [])
 
   const addPerson = (event) => {
     event.preventDefault()
-    const person = persons.find((p) => {
-      return p.name.toLowerCase() === newName.toLowerCase()
-    })
-    if(person) {
+    const personObject = {
+      name: newName,
+      number: newNumber
+    }
+    const person = persons.find(
+      (p) => p.name.toLowerCase() === personObject.name.toLowerCase()
+    )
+    if (person) {
       alert("Пользователь с таким именем уже существует")
     } else {
-      setPersons([...persons, {name: newName}])
-      setNewName('')
+      createPost(personObject).then(() => {
+        setNewNumber("")
+        setNewName("")
+        getContacts().then((res) => setPersons(res.data))
+      })
     }
   }
 
@@ -80,9 +51,11 @@ function App() {
       <h1>Пользователи</h1>
       <form className='pers' onSubmit={addPerson}>
         <input type="text"
-        value={newName}
-        onChange={event => setNewName(event.target.value)} placeholder="person" />
-        <input type="text" placeholder="телефон" />
+          value={newName}
+          onChange={event => setNewName(event.target.value)} placeholder="person" />
+        <input type="text"
+          value={newNumber}
+          onChange={event => setNewNumber(event.target.value)} placeholder="телефон" />
         <input type="submit" value="Добавить пользователя" />
       </form>
       <div>
